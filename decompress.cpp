78279defa7febb0de1a48baf0ee484e1546401cd
Paul <102178068+PaulsGitHubs@users.noreply.github.com>
2023-05-20 02:36:04 -0700

    Update decompress.cpp

--- decompress.cpp
+++ decompress.cpp
@@ -7,33 +7,43 @@ int main() {
     // Read the real part from binary file
     double* dwt_real = new double[N];
     std::ifstream input_real("dwt_real.bin", std::ios::binary);
-    if(input_real.is_open()) {
-        input_real.read((char*)dwt_real, sizeof(double) * N);
+    if(!input_real.is_open()) {
+        std::cerr << "Failed to open dwt_real.bin" << std::endl;
+        return 1;
     }
+    input_real.read((char*)dwt_real, sizeof(double) * N);
     input_real.close();
 
     // Read the imaginary part from binary file
     double* dwt_imag = new double[N];
     std::ifstream input_imag("dwt_imag.bin", std::ios::binary);
-    if(input_imag.is_open()) {
-        input_imag.read((char*)dwt_imag, sizeof(double) * N);
+    if(!input_imag.is_open()) {
+        std::cerr << "Failed to open dwt_imag.bin" << std::endl;
+        return 1;
     }
+    input_imag.read((char*)dwt_imag, sizeof(double) * N);
     input_imag.close();
 
     // Perform inverse discrete wavelet transform on real part
-    wave_object obj_real;
-    wt_object wt_real;
-    obj_real = wave_init("db4"); // Initialize wavelet
-    wt_real = wt_init(obj_real, "idwt", N, 4); // Initialize inverse discrete wavelet transform object with 4 levels
+    wave_object obj_real = wave_init("db4"); // Initialize wavelet
+    if(obj_real == NULL) {
+        std::cerr << "Failed to initialize wavelet for real part" << std::endl;
+        return 1;
+    }
+
+    wt_object wt_real = wt_init(obj_real, "idwt", N, 4); // Initialize inverse discrete wavelet transform object with 4 levels
     setDWTExtension(wt_real, "sym");  // Set extension mode
     setWTConv(wt_real, "direct");  // Set convolution type
     idwt(wt_real, dwt_real); // Perform IDWT on real part
 
     // Perform inverse discrete wavelet transform on imaginary part
-    wave_object obj_imag;
-    wt_object wt_imag;
-    obj_imag = wave_init("db4"); // Initialize wavelet
-    wt_imag = wt_init(obj_imag, "idwt", N, 4); // Initialize inverse discrete wavelet transform object with 4 levels
+    wave_object obj_imag = wave_init("db4"); // Initialize wavelet
+    if(obj_imag == NULL) {
+        std::cerr << "Failed to initialize wavelet for imaginary part" << std::endl;
+        return 1;
+    }
+
+    wt_object wt_imag = wt_init(obj_imag, "idwt", N, 4); // Initialize inverse discrete wavelet transform object with 4 levels
     setDWTExtension(wt_imag, "sym");  // Set extension mode
     setWTConv(wt_imag, "direct");  // Set convolution type
     idwt(wt_imag, dwt_imag); // Perform IDWT on imaginary part
