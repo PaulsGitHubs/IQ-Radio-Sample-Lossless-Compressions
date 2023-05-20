0445d130dfcf08670cb17aeb496651e980686c67
Paul <102178068+PaulsGitHubs@users.noreply.github.com>
2023-05-20 02:29:07 -0700

    Update rtl_capture.c

--- rtl_capture.c
+++ rtl_capture.c
@@ -1,18 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <rtl-sdr.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define BUF_LEN (8192)
 
+rtlsdr_dev_t *dev = NULL;
+FILE *file = NULL;
+
 void rtl_callback(unsigned char *buf, uint32_t len, void *ctx) {
     FILE *file = (FILE *)ctx;
-
-    // Write the data to the file.
     fwrite(buf, 1, len, file);
 }
 
+void* listen_for_input(void* arg) {
+    char c;
+    while (1) {
+        c = getchar();
+        if (c == ' ' || c == '\n' || c == 'c') {
+            break;
+        }
+    }
+    exit(0); // Terminates the program if any of the exit keys are pressed.
+}
+
+void cleanup(int sig_num) {
+    printf("\nClosing...\n");
+
+    // Stop the asynchronous reader.
+    if (dev != NULL) {
+        rtlsdr_cancel_async(dev);
+    }
+
+    // Close the RTL-SDR device.
+    if (dev != NULL) {
+        rtlsdr_close(dev);
+    }
+
+    // Close the output file.
+    if (file != NULL) {
+        fclose(file);
+    }
+    exit(0);
+}
+
 int main() {
-    rtlsdr_dev_t *dev = NULL;
+    struct sigaction sa;
+    sa.sa_handler = cleanup;
+    sigaction(SIGINT, &sa, NULL);
+
+    pthread_t input_thread;
+    pthread_create(&input_thread, NULL, listen_for_input, NULL);
+
     int dev_index = 0; // Use the first RTL-SDR device by default.
 
     // Open the RTL-SDR device.
@@ -36,11 +77,13 @@ int main() {
     }
 
     // Open the output file.
-    FILE *file = fopen("input.bin", "wb");
+    file = fopen("input.bin", "wb");
     if (file == NULL) {
         fprintf(stderr, "Failed to open output file.\n");
         return 1;
     }
+
+    printf("To stop the program, press 'Spacebar', 'Enter', 'c' or 'Ctrl+C'\n");
 
     // Start the RTL-SDR's asynchronous reader.
     if (rtlsdr_read_async(dev, rtl_callback, file, 0, BUF_LEN) < 0) {
@@ -48,11 +91,6 @@ int main() {
         return 1;
     }
 
-    // Close the RTL-SDR device.
-    rtlsdr_close(dev);
-
-    // Close the output file.
-    fclose(file);
-
+    pthread_join(input_thread, NULL); // Wait for the input thread to finish
     return 0;
 }
